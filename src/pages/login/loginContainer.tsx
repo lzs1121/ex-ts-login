05e38cdeda7c32d3680d6a084e75abf6b62b6eb2
Zishuo Li <zishuoli@Zishuos-MacBook-Pro.local>
2020-01-06 16:28:43 +1100

    first commit

---
+++ src/pages/login/loginContainer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const LoginComponent = () => {
+  return (
+    <div>
+      <h2>Login Form</h2>
+    </div>
+  );
+};
---
+++ src/pages/student-list/studentListContainer.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const StudentListComponent = () => {
+  return <h2>Student List Page</h2>;
+};
